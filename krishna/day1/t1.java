fb0df72d13a6a48c6bf29ab5513c9a6ba8c6750d
krishnasuratwala <s21_suratwala_krishna@mgmcen.ac.in>
2023-03-27 00:19:49 +0530

    day-1p-1
    alternate problem solution

--- krishna/day1/t1.java
+++ krishna/day1/t1.java
@@ -15,3 +15,26 @@ class Solution {
      return new int[0];
     }
 }
+//alternate solution
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+          int[] array=new int[2];
+          int k;
+          for(int i=0;i<nums.length;i++){
+               if(nums[i]<=target){
+                k=target-nums[i];
+                for(int j=i+1;j<nums.length;j++){
+                     if(nums[j]==k){
+                          array[0]=i;
+                          array[1]=j;
+                          return array;
+                     }
+                }    
+               }
+              
+          }
+           return new int[0];
+               
+          }
+     }   
+   
